7a09913ca59f919d953c54aaf4b1f709f56d5abc
Валаев Георгий <ymnikgoga@gmail.com>
2025-03-25 22:48:01 +0300

    класс селениумовских методов

---
+++ src/main/java/steps/SeleniumSteps.java
@@ -0,0 +1,16 @@
+package steps;
+
+import org.openqa.selenium.By;
+
+import static drivermanager.DriverManager.getDriver;
+
+public class SeleniumSteps {
+
+    public static void openPage(String page) {
+        getDriver().get(page);
+    }
+
+    public static void clickButton(By locator) {
+        getDriver().findElement(locator).click();
+    }
+}

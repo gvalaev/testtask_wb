770e73e298b5d336767bb90689fcdce069fc80ee
Валаев Георгий <ymnikgoga@gmail.com>
2025-03-25 21:44:59 +0300

    драйвер менеджер

---
+++ src/main/java/drivermanager/DriverManager.java
@@ -0,0 +1,92 @@
+package drivermanager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.Objects;
+
+public class DriverManager {
+    private static WebDriver driver;
+    private static WebDriverWait driverWait;
+
+    /**
+     * Инициализация Вебдрайвера в зависимости от переданного имени браузера
+     */
+    private static void init(String browserName) {
+        Objects.requireNonNull(browserName, "Имя браузера не должно быть пустым");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless", "--disable-dev-shm-usage", "--ignore-certificate-errors");
+        String driverFileName = switch (browserName.toLowerCase()) {
+            case "chrome" -> "chromedriver.exe";
+            case "yandex" -> "yandexdriver.exe";
+            default -> throw new IllegalArgumentException("Не знаю такого браузера: " + browserName);
+        };
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\src\\main\\resources\\webdrivers\\" + driverFileName);
+        driver = new ChromeDriver(chromeOptions);
+    }
+
+    /**
+     * Геттер вебдрайвера
+     *
+     * @return объект драйвера
+     */
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    /**
+     * Сеттер вебдрайвера
+     *
+     * @param browserName обозначение браузера
+     */
+    public static void setDriver(String browserName) {
+        if (driver == null) {
+            init(browserName);
+        }
+    }
+
+    /**
+     * Закрыть все сессии браузера
+     */
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            System.out.println(Thread.currentThread().getId());
+        }
+    }
+
+    /**
+     * Инициализация Вебдрайвервейта
+     *
+     * @return объект вебдрайвервейта
+     */
+    private static WebDriverWait initWait() {
+        Duration duration = Duration.ofSeconds(10);
+        return new WebDriverWait(driver, duration);
+    }
+
+    /**
+     * Геттер для вебдрайвервейта
+     *
+     * @return объект вебдрайвервейта
+     */
+    public static WebDriverWait getDriverWait() {
+        if (driverWait == null) {
+            throw new IllegalStateException("Драйвер ожидания не был проинициализирован");
+        }
+        return
+                driverWait;
+    }
+
+    /**
+     * Присваиваем ссылку объекту вебдрайвервейта
+     */
+    public static void setDriverWait() {
+        if (driverWait == null) {
+            driverWait = initWait();
+        }
+    }
+}

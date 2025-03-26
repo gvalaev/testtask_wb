bee1557ae00ecf9904f3de2bac4975e52d12ede8
Валаев Георгий <ymnikgoga@gmail.com>
2025-03-26 22:00:54 +0300

    перепилен метод проверки

--- src/main/java/steps/CheckSteps.java
+++ src/main/java/steps/CheckSteps.java
@@ -1,12 +1,16 @@
 package steps;
 
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
-import static drivermanager.DriverManager.getDriver;
+import static drivermanager.DriverManager.getDriverWait;
 
 public class CheckSteps {
 
-    public static void checkIfButtonActive(By locator) {
-        getDriver().findElement(locator).getAttribute("class").contains("current");
+    public static void checkIfElementIsDisplayed(By locator) {
+        WebElement webElement = getDriverWait().until(ExpectedConditions.visibilityOfElementLocated(locator));
+        Assertions.assertTrue(webElement.isDisplayed());
     }
 }
